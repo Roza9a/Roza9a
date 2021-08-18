497acb50ce64e0f57ff0c533c20712c7b0fef452
MRCROW <88156055+MRCROW88@users.noreply.github.com>
2021-08-18 22:16:05 +0300

    Update radio.js

--- Music/radio.js
+++ Music/radio.js
@@ -2,7 +2,7 @@
   //////CONFIG LOAD///////////
   ////////////////////////////
   const { canModifyQueue } = require("../util/MilratoUtil");
-  const { play } = require("../include/play");
+  const { play } = require("../music/play");
   const { attentionembed } = require("../util/attentionembed"); 
   const { MessageEmbed } = require("discord.js");
   const Discord = require("discord.js");
