4969491477f7304ed981bff791e72ac6003df1d4
MRCROW <88156055+MRCROW88@users.noreply.github.com>
2021-08-19 15:48:24 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,123 +1,104 @@
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const {
-  PREFIX,
-  approveemoji,
-  denyemoji
+const { 
+  PREFIX, 
 } = require(`../config.json`);
-const db = require('quick.db');
 
 module.exports = {
-  name: `~musiccommands`,
-  description: "(h)Gives you a list of all help Commands",
+  name: `help`,
+  description: `Gives you a list of all help Commands`,
   aliases: ["h","commands"],
   cooldown: 3,
   edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
-  async execute(message,args,client) {
-
-    let prefix = await db.get(`prefix_${message.guild.id}`)
-    if(prefix === null) prefix = PREFIX;
-    //react with approve emoji
-    message.react("769665713124016128");
-    //define the commands as a command
+  execute(message,args,client) {
+     
     let commands = message.client.commands.array();
-    //define the help embed
-    let resultsEmbed = new Discord.MessageEmbed()
-      .setTitle(`List of all commands`)
-       .setImage('https://cdn.discordapp.com/attachments/877688129463664690/877891416209121280/92d9607b947fbd8c44a0df1f3c582d71.gif')
-      .setThumbnail(message.author.avatarURL({dynamic: "true"}))
-      .setColor('RANDOM');
-       //define argstrue to negative
-    let ifargstruedothis = -1;
+ 
+    let helpEmbed = new MessageEmbed()
+      .setTitle("MBWF-v1 Help")
+      .setDescription(`**Version:** \`v2.8\` \n**PREFIX:** \`${+}\``)
+      .setFooter( client.user.username +`Type: ${+}help <Command>  for more information!`)
+      .setColor("#c219d8");
 
+      let ifargstruedothis = -1;
+      
       switch(args[0]){
           case "filter":
            ifargstruedothis=0;
           break;
           case "loop":
-            ifargstruedothis=0.1;
+            ifargstruedothis=1;
           break;
           case "lyrics":
-            ifargstruedothis=0.3
-          break;
-          case "nowplaying":
-            ifargstruedothis=0.4
-          break;
-          case "pause":
-            ifargstruedothis=0.5
-          break;
-          case "play":
-            ifargstruedothis=1
-          break;
-          case "playlist":
             ifargstruedothis=2
           break;
-          case "queue":
+          case "nowplaying":
             ifargstruedothis=3
           break;
-          case "radio":
+          case "pause":
             ifargstruedothis=4
           break;
-          case "remove":
+          case "play":
             ifargstruedothis=5
-          break; 
-          case "resume":
+          break;
+          case "playlist":
             ifargstruedothis=6
           break;
-          case "search":
+          case "queue":
             ifargstruedothis=7
           break;
-          case "shuffle":
+          case "radio":
             ifargstruedothis=8
           break;
-          case "skip":
+          case "remove":
             ifargstruedothis=9
           break;
-          case "skipto":
+          case "resume":
             ifargstruedothis=10
           break;
-          case "stop":
+          case "search":
             ifargstruedothis=11
           break;
-          case "volume":
+          case "shuffle":
             ifargstruedothis=12
           break;
-          case "botlist":
+          case "skip":
             ifargstruedothis=13
           break;
-          case "help":
+          case "skipto":
             ifargstruedothis=14
           break;
-          case "invite":
+          case "stop":
             ifargstruedothis=15
           break;
-          case "ping":
+          case "volume":
             ifargstruedothis=16
           break;
-          case "prefix":
+          case "help":
             ifargstruedothis=17
           break;
-          case "uptime":
-            ifargstruedothis=18
-          break;
-          default:
+          default:        
             commands.forEach((cmd) => {
               helpEmbed.addField(
-                `**${prefix}${cmd.name}**`,
+                `**${message.client.prefix}${cmd.name}**`,
                 `${cmd.description}`,
                 true
               );
             });
-    
           if(!message.guild) {
-            if(!args[0]) {message.react(approveemoji);return message.channel.send(helpEmbed);}
+            if(!args[0]) {message.react("<:Yeh:827947991922376776>");return message.author.send(helpEmbed);}
             return
             }
-            message.react(approveemoji);
-message.channel.send(helpEmbed)
-
+            message.react("<:Yeh:827947991922376776>");
+            message.author.send(new MessageEmbed().setColor("#c219d8")
+            .setDescription(`** <:Ba:827947991632969749> Sent from <#${message.channel.id}>**`))
+            message.author.send(helpEmbed)
+            message.channel.send( new MessageEmbed().setColor("#c219d8")
+            .setDescription(`** <:Ba:827947991632969749> ${message.author} Check your \`direct messages\` for a list of Commands!**`)
+            );
+           
         break;
        }
-
+     
        if(ifargstruedothis>=0){
          let aliases = commands[ifargstruedothis].aliases;
          if(aliases === undefined || !aliases) aliases="No Aliases!";
@@ -126,7 +107,7 @@ message.channel.send(helpEmbed)
 
 
         helpEmbed.addField(
-          `**${prefix}${commands[ifargstruedothis].name}**`,
+          `**${message.client.prefix}${commands[ifargstruedothis].name}**`,
           `\`\`\`fix\n${commands[ifargstruedothis].edesc}\n\`\`\`\n\`${commands[ifargstruedothis].description}\``
         );
         helpEmbed.addField(
@@ -140,8 +121,9 @@ message.channel.send(helpEmbed)
         if(!message.guild) return message.author.send(helpEmbed);
           message.author.send(helpEmbed)
           message.channel.send( new MessageEmbed().setColor("#c219d8")
-        .setDescription(`**📩 ${message.author} Check your \`direct messages\` for a list of Commands!**`)
+          .setDescription(`** <:Ba:827947991632969749> ${message.author} Check your \`direct messages\` for a list of Commands!**`)
           );
        }
 
 }
+} 
