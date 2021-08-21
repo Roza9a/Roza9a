d6d4d4dac82a789277090778dbf50bf32ce3f2d7
MRCROW <88156055+MRCROW88@users.noreply.github.com>
2021-08-21 20:31:31 +0300

    Update uptime.js

--- others/uptime.js
+++ others/uptime.js
@@ -18,7 +18,7 @@ module.exports = {
     let seconds = Math.floor(client.uptime / 1000) % 60;
     //react with approve emoji
     message.react("769665713124016128");
-    return message.channel.send(new MessageEmbed().setColor("#c219d8").setTitle(`**__CHILL BOT UPTIME__**\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
+    return message.channel.send(new MessageEmbed().setColor("#8AD0DA").setTitle(`**__CHILL BOT UPTIME__**\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
 
 
   }
