8707eac566925931f901cb7332ee070feceb2881
MRCROW <88156055+MRCROW88@users.noreply.github.com>
2021-08-21 19:25:31 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -33,7 +33,7 @@ execute(message) {
     queue.connection.dispatcher.end();
     //send approve message
     queue.textChannel.send(
-      new MessageEmbed().setColor("#c219d8").setAuthor(`${message.author.username} skipped the song.`, "https://cdn.discordapp.com/emojis/769915194444480542.png")
+      new MessageEmbed().setColor("#c219d8").setAuthor(`${message.author.username} skipped the song.`, "https://media.discordapp.net/attachments/807677211133083698/878676128129237032/OIP_13.jpg?width=473&height=473")
     ).catch(console.error);
   }
 };
