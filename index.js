9a41998f9688d4281f431316f4f62dfdd19cb188
MRCROW <88156055+MRCROW88@users.noreply.github.com>
2021-08-21 21:47:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ client.on(`ready`, () => {
         { return member.voice.channel.leave(); }
       });
       
-
-    client.user.setActivity(`Type: ${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: ""});
    
   
       }, (5000));
